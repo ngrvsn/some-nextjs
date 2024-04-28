ff8b3f8c2f4a5ef929be86b238a5536620e110ce
alex_solovyov <alex.solovyov19@gmail.com>
2024-04-28 13:38:42 +0500

    Добавил глобал скрипт с яндекс метрикой

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { Montserrat } from 'next/font/google'
+import Script from 'next/script'
+import Image from 'next/image'
 import { ToastContainer } from 'react-toastify'
 import { Wrapper } from '@/components/layout/Wrapper/Wrapper'
 import type { Metadata } from 'next'
@@ -36,6 +38,36 @@ export default function RootLayout({
           autoClose={5000}
           hideProgressBar
         />
+        <Script
+          strategy='afterInteractive'
+          id='yandex-metrika'
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          ym(97057404, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+          });
+          `
+          }}
+        />
+        <noscript>
+          <div>
+            <Image
+              src='https://mc.yandex.ru/watch/97057404'
+              width={1}
+              height={1}
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=''
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   )
