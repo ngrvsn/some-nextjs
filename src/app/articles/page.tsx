e2e5db4fe6c92bf8ea10b4bf26cf16570d157266
ngrvsn <forpomoika2@gmail.com>
2024-06-08 13:00:09 +0300

    фикс

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -11,7 +11,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default function Articles() {
   return (
     <main>
-      <ArticleWrapper />
+      <ArticleWrapper articleSlug={''} articleTitle={''} />
     </main>
   )
 }
